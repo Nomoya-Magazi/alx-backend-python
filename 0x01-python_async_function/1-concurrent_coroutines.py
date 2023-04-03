dca831d808e0b0ed7a6feec12b6d35e125dba9f1
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-04-03 14:45:59 +0000

    fixed error

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-
 '''task 1 module
 '''
-
-wait_random = __import__('0-basic_async_syntax').wait_random
-
 from typing import List
 import asyncio
+
+ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
@@ -15,4 +13,3 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     results = await asyncio.gather(*tasks)
     return sorted(results)
-
