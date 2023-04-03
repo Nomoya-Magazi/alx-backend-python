2463a1251cf315bb91ce2d5b3efd6893f49334b2
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-04-03 14:55:25 +0000

    async funtion

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 '''task 2 module
 '''
+import time
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-import time
 
 def measure_time(n: int, max_delay: int) -> float:
     '''gives the avarage runtime of wait_n
     '''
+
     start_time = time.time()
     wait_n(n, max_delay)
     total_time = time.time() - start_time
