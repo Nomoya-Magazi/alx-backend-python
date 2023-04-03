4aa38bc7234ec32603c2f78ab9320ad62beddca7
Nomoya Magazi <nomoya.magazi@gmail.com>
2023-04-03 14:41:38 +0000

    async functions

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,8 +7,8 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''waits for a random number of seconds
+    '''waits for a random delay between 0 and max_delay
     '''
-    wait_time = random.random() * max_delay
-    await asyncio.sleep(wait_time)
-    return wait_time
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -4,13 +4,12 @@
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
-import asyncio
 import time
 
 def measure_time(n: int, max_delay: int) -> float:
     '''gives the avarage runtime of wait_n
     '''
-    start_time =time.time()
+    start_time = time.time()
     wait_n(n, max_delay)
     total_time = time.time() - start_time
     return total_time / n
