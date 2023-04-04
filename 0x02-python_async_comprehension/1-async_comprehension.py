7d4f7a449b4f32eb539c8b62339643a37f602440
Nomoya-Magazi <105003971+Nomoya-Magazi@users.noreply.github.com>
2023-04-04 14:30:45 +0200

    async

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -10,5 +10,5 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[int]:
     '''creates a list of 10 numbers
     '''
-    result = [num async for num in async_generator()]
+    result = [value async for value in async_generator()]
     return result
